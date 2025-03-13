b3db7075e175de867dac561bac647ce1405461dd
sort9 <143142285+sort9@users.noreply.github.com>
2025-03-12 22:33:11 -0400

    Create lists.py

---
+++ src/lists.py
@@ -0,0 +1,17 @@
+# List where the next number is the result of the function n = n + 1
+list1 = [1, 2, 3, 4, 5]
+
+# List where the next number is the result of the function n = n + 2
+list2 = [2, 4, 6, 8, 10]
+
+# List where the next number is the result of the function an = 3^2(n-1)
+list3 = [3, 9, 81, 6561, 43046721] # sequence = [3**(2**(n-1)) for n in range(1, 6)]
+
+# List where the next number is the result of the function an = ((n * 10)^2) - 2
+list4 = [98, 398, 898, 1598, 2498] # sequence = [(((n*10)**2) - 2) for n in range(1, 6)]
+
+# List where the next number is the result of the function an = (7 * (n * 3))
+list5 = [21, 42, 63, 84, 105]
+
+# List where the next number is the result of the function an = (7 * (n * 3))
+incompletelist = [2, 6, 10, 14]
